e7fbe5ff2dbcf4227fa9e428e839aafebe6e4d24
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-20 18:58:29 -0500

    adding and implementing mocks

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,21 +1,28 @@
+import { fireEvent } from '@testing-library/react';
+
 /*
   We are rendering `<Application />` down below, so we need React.createElement
 */
-import React from "react";
+import React from 'react';
 
 /*
   We import our helper functions from the react-testing-library
   The render function allows us to render Components
 */
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
 /*
   We import the component that we are testing
 */
-import Application from "../Application";
+import Application from '../Application';
 /*
   A test that renders a React Component
 */
-it("renders without crashing", () => {
-  render(<Application />);
+it('defaults to Monday and changes the schedule when a new day is selected', () => {
+  const { queryByText, findByText } = render(<Application />);
+
+  return findByText('Monday').then(() => {
+    fireEvent.click(queryByText('Tuesday'));
+    expect(queryByText('Leopold Silvers')).toBeInTheDocument();
+  });
 });
