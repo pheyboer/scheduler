eccb8e943d160656be98b04abb0d7e29d79a554c
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-25 18:52:30 -0500

    implementing tests for loads data, cancels an interview and increases the spots remaining for Monday by 1

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -84,12 +84,24 @@ it('loads data, books an interview and reduces the spots remaining for monday by
 });
 
 it('loads data, cancels an interview and increases the spots remaining for Monday by 1', async () => {
+
   // 1. Render the Application.
+  const { container, debug } = render(<Application />);
+
   // 2. Wait until the text "Archie Cohen" is displayed.
+  await findByText(container, "Archie Cohen");
+
   // 3. Click the "Delete" button on the booked appointment.
+  const appointment = getAllByTestId(container, "appointment").find((appointment) =>
+    queryByText(appointment, "Archie Cohen")
+  );
+
+  fireEvent.click(queryByAltText(appointment, "Delete"));
   // 4. Check that the confirmation message is shown.
   // 5. Click the "Confirm" button on the confirmation.
   // 6. Check that the element with the text "Deleting" is displayed.
   // 7. Wait until the element with the "Add" button is displayed.
   // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+
+  debug();
 });
