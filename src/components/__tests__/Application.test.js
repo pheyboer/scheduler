de81c4907bf32f4caf4655ddaa84a030fe66404e
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-26 13:11:38 -0500

    implementing test for shows the save error when failing to save an appointment

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -118,7 +118,6 @@ it('loads data, cancels an interview and increases the spots remaining for Monda
     queryByText(day, 'Monday')
   );
   expect(getByText(day, '2 spots remaining')).toBeInTheDocument();
-
 });
 
 /*
@@ -166,15 +165,31 @@ it('loads data, edits an interview and keeps the spots remaining for Monday the 
   expect(getByText(day, '1 spot remaining')).toBeInTheDocument();
 });
 
-it('shows the save error when failing to save an appointment', () => {
-  axios.put.mockRejectedValueOnce();
+it('shows the save error when failing to save an appointment', async () => {
+  axios.put.mockRejectedValueOnce(new Error("Failed to save appointment"));
   //1 render the application
+  const { container } = render(<Application />);
 
-  //2
+  //2 Wait until the text "Archie Cohen" is displayed (data loaded)
+  await findByText(container, 'Archie Cohen');
 
-  //3
+  //3 Click on edit button on the booked appointment
+  const appointment = getAllByTestId(container, 'appointment').find((appointment) =>
+    queryByText(appointment, 'Archie Cohen')
+  );
+  fireEvent.click(getByAltText(appointment, 'Edit'));
 
-  //4
+  //4 Edit student name
+
+  //5 Select interviewer
+
+  //6 Click save button
+
+  //7 Check for error message 
+
+  //8 Check close button and hides the error message
+
+
 });
 
 it('shows the delete error when failing to delete an existing appointment', () => {
