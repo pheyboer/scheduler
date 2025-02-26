26976135bc8e2e22a074d2d207e6e2bbcc2e2f51
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-25 19:55:23 -0500

    updating and fixing test for loads data, books an interview and reduces the spots remaining for monday by 1

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,6 +6,7 @@ import {
   getAllByTestId,
   getByAltText,
   queryByText,
+  queryByAltText,
 } from '@testing-library/react';
 
 import '@testing-library/jest-dom';
@@ -23,7 +24,6 @@ import { render } from '@testing-library/react';
 
 import axios from 'axios';
 jest.mock('axios');
-
 
 /*
   We import the component that we are testing
@@ -84,24 +84,40 @@ it('loads data, books an interview and reduces the spots remaining for monday by
 });
 
 it('loads data, cancels an interview and increases the spots remaining for Monday by 1', async () => {
-
   // 1. Render the Application.
   const { container, debug } = render(<Application />);
 
   // 2. Wait until the text "Archie Cohen" is displayed.
-  await findByText(container, "Archie Cohen");
+  await findByText(container, 'Archie Cohen');
 
   // 3. Click the "Delete" button on the booked appointment.
-  const appointment = getAllByTestId(container, "appointment").find((appointment) =>
-    queryByText(appointment, "Archie Cohen")
+  const appointment = getAllByTestId(container, 'appointment').find(
+    (appointment) => queryByText(appointment, 'Archie Cohen')
   );
 
-  fireEvent.click(queryByAltText(appointment, "Delete"));
+  fireEvent.click(queryByAltText(appointment, 'Delete'));
+
   // 4. Check that the confirmation message is shown.
+  expect(
+    getByText(appointment, 'Are you sure you would like to delete?')
+  ).toBeInTheDocument();
+
   // 5. Click the "Confirm" button on the confirmation.
+  fireEvent.click(getByText(appointment, "Confirm"));
+
   // 6. Check that the element with the text "Deleting" is displayed.
+
   // 7. Wait until the element with the "Add" button is displayed.
+
   // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
 
   debug();
 });
+
+/*
+tests:
+"loads data, cancels an interview and increases the spots remaining for Monday by 1"
+"loads data, edits an interview and keeps the spots remaining for Monday the same"
+"shows the save error when failing to save an appointment"
+"shows the delete error when failing to delete an existing appointment"
+*/
