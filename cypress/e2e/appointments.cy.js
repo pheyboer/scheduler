ece210970f9b669e36b3a02446079a84f645ef40
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-27 13:13:58 -0500

    implementing appointment tests with cypress

--- cypress/e2e/appointments.cy.js
+++ cypress/e2e/appointments.cy.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Appointments', () => {
+  it('should book an interview', () => {
+    //reset database
+    cy.request('GET', '/api/debug/reset');
+    //visit root of web server
+    cy.visit('/');
+    //confirm DOM contains the text monday
+    cy.contains('Monday');
+    //click add button second appointment
+    cy.get('[alt=Add]').first().click();
+    //enter name
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+    //choose interviewer
+    cy.get("[alt='Sylvia Palmer']").click();
+    //click the save button
+    cy.contains('Save').click();
+    //verification
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Sylvia Palmer');
+  });
+});
