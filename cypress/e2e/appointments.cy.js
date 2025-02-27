3c8ae23fbb7aa8acd7a2ff192295de63cd2a946f
Peter Heyboer <heyboer.peter@gmail.com>
2025-02-27 13:28:25 -0500

    implementing appointment tests with cypress

--- cypress/e2e/appointments.cy.js
+++ cypress/e2e/appointments.cy.js
@@ -1,11 +1,38 @@
+// describe('Appointments', () => {
+//   it('should book an interview', () => {
+//     //reset database
+//     cy.request('GET', '/api/debug/reset');
+//     //visit root of web server
+//     cy.visit('/');
+//     //confirm DOM contains the text monday
+//     cy.contains('Monday');
+//     //click add button second appointment
+//     cy.get('[alt=Add]').first().click();
+//     //enter name
+//     cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+//     //choose interviewer
+//     cy.get("[alt='Sylvia Palmer']").click();
+//     //click the save button
+//     cy.contains('Save').click();
+//     //verification
+//     cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+//     cy.contains('.appointment__card--show', 'Sylvia Palmer');
+//   });
+// });
+
+//refactored to separate common test commands from ones specific to the shoold book an appointment interview
+
 describe('Appointments', () => {
-  it('should book an interview', () => {
+  beforeEach(() => {
     //reset database
     cy.request('GET', '/api/debug/reset');
     //visit root of web server
     cy.visit('/');
     //confirm DOM contains the text monday
     cy.contains('Monday');
+  });
+
+  it('should book an interview', () => {
     //click add button second appointment
     cy.get('[alt=Add]').first().click();
     //enter name
@@ -17,5 +44,30 @@ describe('Appointments', () => {
     //verification
     cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
     cy.contains('.appointment__card--show', 'Sylvia Palmer');
+  });
+
+  it('should edit an interview', () => {
+    //click edit button
+    cy.get('[alt=Edit]').first().click({ force: true });
+    //enter name
+    cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
+    //choose interviewer
+    cy.get("[alt='Tori Malcolm']").click();
+    //click the save button
+    cy.contains('Save').click();
+    //verification
+    cy.contains('.appointment__card--show', 'Lydia Miller-Jones');
+    cy.contains('.appointment__card--show', 'Tori Malcolm');
+  });
+
+  it('should cancel an interview', () => {
+    //click cancel button
+    cy.get('[alt=Delete]').first().click({ force: true });
+    //click the confirm button
+    cy.contains('Confirm').click();
+    //deleting indicator
+    cy.contains('Deleting').should('exist');
+    cy.contains('Deleting').should('not.exist');
+    cy.contains('.appointment__card--show', 'Archie Cohen').should('not.exist');
   });
 });
